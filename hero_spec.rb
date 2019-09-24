d90ff43efc785273fade178571899014fe727f69
denis281542 <deniskuimov@mail.ru>
2019-09-24 22:53:59 +0500

    Added test1

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,16 +1,6 @@
 require './hero'
 
-describe Hero do  
-  it 'has a capitalize name' do
-    hero = Hero.new 'mike'
-
-    expect(hero.name).to eq 'Mike'
-  end  
-end
-
-require './hero'
-
-describe Hero do  
+describe Hero do 	
   it 'has a capitalize name' do
     hero = Hero.new 'mike'
 
