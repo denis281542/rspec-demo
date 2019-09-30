914eddd72e4263e29d30b6b3d6a0e14644208369
denis281542 <deniskuimov@mail.ru>
2019-09-30 18:05:44 +0500

    Def heg

--- hero_spec.rb
+++ hero_spec.rb
@@ -31,14 +31,4 @@ describe Hero do
 end
 
 
-require './hero'
-
-describe Hero do  
-  it 'has a capitalize name' do
-    hero = Hero.new 'mike'
-
-    expect(hero.name).to eq 'Mike'
-  end  
-end
-
-
+123
